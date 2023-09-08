a6d172d826c2dae25a184ee15a00c6052666c5f8
nicobonik <39326876+nicobonik@users.noreply.github.com>
2023-09-08 12:40:18 -0400

    made some updates so i dont get mismatch version issues

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -3,7 +3,7 @@ local lsp = require('lsp-zero')
 lsp.preset('recommended')
 
 lsp.ensure_installed({
-    'lua_ls',
+    'sumneko_lua',
     'rust_analyzer'
 
 })
@@ -13,13 +13,11 @@ local cmp_select = {behavior = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
     ['C-p'] = cmp.mapping.select_prev_item(cmp_select),
     ['C-n'] = cmp.mapping.select_next_item(cmp_select),
-    ['C-y'] = cmp.mapping.confirm({select = true})
+    ['CR'] = cmp.mapping.confirm({select = false})
 })
-
 
 lsp.setup_nvim_cmp({
     mapping = cmp_mappings
 })
 
 lsp.setup()
-
--- lua/nicobonik/remap.lua
+++ lua/nicobonik/remap.lua
@@ -1,2 +1,8 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>..", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>..", vim.cmd.Ex) --exit file into filetree
+vim.keymap.set("n", "<leader>ua", function() --update all
+    vim.cmd('PackerSync')
+    vim.cmd('TSUpdate')
+    --add any other plugin update things here (Mason maybe?)
+end)
+
