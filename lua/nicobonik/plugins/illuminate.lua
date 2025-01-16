68f957c04cf4ceedd8362c209260b4eb6fbded57
nicobonik <39326876+nicobonik@users.noreply.github.com>
2025-01-16 14:14:43 -0500

    update and add plugins

--- lua/nicobonik/plugins/illuminate.lua
+++ lua/nicobonik/plugins/illuminate.lua
@@ -7,7 +7,7 @@ return {
       -- priority
       providers = { 'lsp', 'treesitter', 'regex', },
       -- delay: delay in milliseconds
-      delay = 1,
+      delay = 100,
       -- filetype_overrides: filetype specific overrides. The keys are strings
       -- to represent the filetype while the values are tables that supports
       -- the same keys passed to .configure except for filetypes_denylist and
--- lua/nicobonik/plugins/nvim-tree.lua
+++ lua/nicobonik/plugins/nvim-tree.lua
@@ -2,6 +2,24 @@ return {
   "nvim-tree/nvim-tree.lua",
 
   config = function ()
-    require("nvim-tree").setup()
+
+    local nvim_tree = require("nvim-tree")
+
+    local api = require("nvim-tree.api")
+
+    local function on_attach(bufnr)
+      -- default mappings
+      api.config.mappings.default_on_attach(bufnr)
+      -- custom mappings
+      vim.keymap.set("n", "T", function()
+	api.node.open.tab_drop()
+	vim.cmd("NvimTreeOpen")
+	vim.cmd("wincmd l")
+      end, { buffer = bufnr, noremap = true, silent = true })
+    end
+
+    nvim_tree.setup({
+      on_attach = on_attach
+    })
   end
 }
